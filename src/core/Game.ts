56aec033205814ceaf2fe0facbd39684dcfe1312
tsongjuls0222 <juls.ablis022@gmail.com>
2022-11-25 16:36:38 +0800

    controllers

--- src/core/Game.ts
+++ src/core/Game.ts
@@ -27,6 +27,16 @@ export default class Game {
   private scene: Scene;
   private diveGroupAnimation: Array<object> = []; 
   private riseGroupAnimation: Array<object> = []; 
+  private plinkogame: Plinko;
+  private slotgame: Slot;
+  private startText: PIXI.Text;
+  private betText: PIXI.Text;
+  private moneyText: PIXI.Text;
+  private gameText: PIXI.Text;
+  private start: Boolean = false;
+  private bet: number = 100;
+  private money: number = 50000;
+  private game: number = 0;
 
   constructor() {
     this.setSettings();
@@ -59,6 +69,7 @@ export default class Game {
 
   private setRenderer() {
     this.main = new PIXI.Application({ width: this.baseWidth, height: this.baseHeight, autoDensity: true });
+    this.main.stage.interactive = true;
     window.document.body.appendChild(this.main.view)
   }
 
@@ -78,17 +89,17 @@ export default class Game {
 
   private createPlinko() {
     this.plinko = new PIXI.Application({ width: this.baseWidth/2, height: this.baseHeight });
-    const plinko = new Plinko(this.plinko,this.main.loader);
-    this.plinko.stage.addChild(plinko.container);
-    this.plinko.stage.addChild(plinko.container2);
+    this.plinkogame = new Plinko(this.plinko,this.main.loader,this.decMoney.bind(this),this.dropOff.bind(this), this.addMoney.bind(this), this.updateGame.bind(this));
+    this.plinko.stage.addChild(this.plinkogame.container);
+    this.plinko.stage.addChild(this.plinkogame.container2);
     this.gameContainer.addChild(this.plinko.stage);
   }
 
   private createSlot() {
     this.slot = new PIXI.Application({ width: this.baseWidth/2, height: this.baseHeight });
     this.slot.loader = this.main.loader;
-    const slot = new Slot(this.slot);
-    this.slot.stage.addChild(slot.container);
+    this.slotgame = new Slot(this.slot);
+    this.slot.stage.addChild(this.slotgame.container);
     this.slot.stage.x = this.baseWidth/2;
     this.slot.stage.y = 140;
     this.gameContainer.addChild(this.slot.stage);
@@ -116,15 +127,15 @@ export default class Game {
     this.mainContainer.addChild(this.sceneContainer);
     this.mainContainer.addChild(this.homeContainer);
     this.mainContainer.addChild(this.gameContainer);
+    this.main.stage.addChild(this.mainContainer);
 
     this.main.stage.addChild(this.mainContainer)
 
     window.addEventListener('keypress', e => this.bonus(e))
+    // start julius code
+    this.createControllers();
+    // end julius code
   }
-
-
-
-
 
   private dive() {
     this.scene.bubbleAnimate();
@@ -163,4 +174,104 @@ export default class Game {
     // this.scene.bubbleAnimate();
   }
 
+  // start julius code
+  private createControllers(){
+    const style = new PIXI.TextStyle({
+        fontFamily: 'Questrial',
+        fontSize: 24,
+        fontWeight: 'bold',
+        fill: '#ffffff',
+    });
+    this.startText = new PIXI.Text('Drop on', style);
+    this.startText.interactive = true;
+    this.startText.buttonMode = true;
+    this.startText.addListener('pointerdown', this.startDrop.bind(this));
+    this.startText.position.y = this.main.screen.height - 50;
+    this.gameContainer.addChild(this.startText);
+
+    //bet
+    this.betText = new PIXI.Text('Bet: '+this.bet, style);
+    this.betText.position.y = this.main.screen.height - 50;
+    this.betText.position.x = 150;
+    this.gameContainer.addChild(this.betText);
+
+    //money
+    this.moneyText = new PIXI.Text('Money: '+this.money, style);
+    this.moneyText.position.y = this.main.screen.height - 50;
+    this.moneyText.position.x = 300;
+    this.gameContainer.addChild(this.moneyText);
+
+    //game
+    this.gameText = new PIXI.Text('Game: '+this.game, style);
+    this.gameText.position.y = this.main.screen.height - 50;
+    this.gameText.position.x = 500;
+    this.gameContainer.addChild(this.gameText);
+  }
+
+  private startDrop(){
+    if(this.bet > this.money){
+      alert("Not enough Money");
+    }
+    else{
+      if(this.plinkogame.up || this.plinkogame.down || this.plinkogame.downleft || this.plinkogame.downright){
+        if(this.start){
+          this.start = false;
+          this.startText.text = 'Drop on';
+          this.plinkogame.startDrop = false;
+        }
+        else{
+          this.start = true;
+          this.startText.text = 'Drop off';
+          this.plinkogame.startDrop = true;
+        }
+    
+        this.checkBall();
+      }
+    }
+  }
+
+  private decMoney(){
+    this.money = this.money - this.bet;
+    this.moneyText.text = 'Money: '+this.money;
+  }
+
+  private dropOff(){
+    if(this.money < this.bet){
+      this.start = false;
+      this.startText.text = 'Drop on';
+      this.plinkogame.startDrop = false;
+
+      this.checkBall();
+    }
+  }
+
+  private checkBall(){
+    if(this.plinkogame.startDrop){
+      if(this.plinkogame.haveBall){
+        this.plinkogame.isDrop = true;
+      }
+    }
+    else{
+      this.plinkogame.isDrop = false;
+    }
+  }
+
+  private addMoney(type: number){
+    if(type == 1){
+      this.money = this.money + (this.bet / 2);
+      this.moneyText.text = 'Money: '+this.money;
+    }
+    else{
+      this.money = this.money + (this.bet * 2);
+      this.moneyText.text = 'Money: '+this.money;
+    }
+  }
+
+  private updateGame(){
+    this.game = this.game + 1;
+    this.gameText.text = 'Game: '+this.game;
+    this.slotgame.getResult();
+  }
+
+  // end julius code
 }
